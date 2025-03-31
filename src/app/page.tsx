a62439fa3b803c0e79b1973255bcd88d97de1d15
Sylph <ngmanhdung2003@gmail.com>
2025-03-31 12:40:19 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,6 @@ import Home2 from "@/modules/home/Home2";
 export default function Home() {
   return (
     <>
-      <Image src="/home/top.png" alt="Logo" width={1920} height={979} />
-      <section className="relative min-h-auto w-full flex flex-row items-center">
-        <div className="absolute top-0 left-0 right-0 bottom-0 h-full p-0 m-0 bg-cover bg-no-repeat bg-center"></div>
-        <div className="relative w-full">{/* <SwiperComponent /> */}</div>
-      </section>
       <Home1 />
       <Home2 />
     </>
