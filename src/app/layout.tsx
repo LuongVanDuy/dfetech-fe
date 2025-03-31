8aec599dc286e62213c9a372b1132bf78332278e
Quang Huy <huytranquang.150701@gmail.com>
2025-03-31 23:23:04 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Link from "next/link";
-import Image from "next/image";
 import DefaultLayout from "@/layouts/DefaultLayout";
 
 const JakartaFont = localFont({
@@ -79,18 +77,6 @@ const JakartaFont = localFont({
     },
   ],
   variable: "--font-custom",
-});
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
 });
 
 export const metadata: Metadata = {
