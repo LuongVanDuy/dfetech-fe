a68d8eb6f75a334297b3903ba63fd16849d1c524
Quang Huy <huytranquang.150701@gmail.com>
2025-03-31 12:32:44 +0700

    update

--- page.tsx
+++
@@ -1,6 +0,0 @@
-import DefaultLayout from "@/layouts/DefaultLayout";
-// import HomePage from "@/modules/home";
-
-export default function Home() {
-  return <DefaultLayout>{/* <HomePage />  */}</DefaultLayout>;
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      boxShadow: {
+        white:
+          "0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -2px rgba(255, 255, 255, 0.1)",
+      },
+      screens: {
+        sm: "375px", // Mobile screens
+        md: "768px", // Tablets
+        lg: "1024px", // Laptops
+        xl: "1280px", // Desktops
+        "2xl": "1536px", // Large desktops
+      },
     },
   },
   plugins: [],
