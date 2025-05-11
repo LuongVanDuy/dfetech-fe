0b1aad5fdcdda1820870583beb044077d51797f1
Nguyen Duy Hung <duyhungfa102@gmail.com>
2025-05-11 15:22:52 +0700

    fix hover label form contact

--- src/components/client/icons/DropDownIcon.tsx
+++ src/components/client/icons/DropDownIcon.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 interface Props {
   className?: string;
+  classNamePath?: string;
 }
 
-const DropDownIcon: React.FC<Props> = ({ className = "" }) => {
+const DropDownIcon: React.FC<Props> = ({ className = "", classNamePath = "" }) => {
   return (
     <svg
       width="23"
@@ -12,6 +13,7 @@ const DropDownIcon: React.FC<Props> = ({ className = "" }) => {
       viewBox="0 0 24 25"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className={className}
     >
       <path
         d="M6 9.54102L12 15.541L18 9.54102"
@@ -19,6 +21,7 @@ const DropDownIcon: React.FC<Props> = ({ className = "" }) => {
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
+        className={classNamePath}
       />
     </svg>
   );
